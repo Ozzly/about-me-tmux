ee09cf9a826d4cf2c4c25a994521333a65133db9
ozzly <gpg@ozzly.net>
2026-04-13 13:27:12 +0100

    style: hover effect on listed files

--- src/components/DirectoryContent.tsx
+++ src/components/DirectoryContent.tsx
@@ -10,7 +10,7 @@ function DirectoryContent({ contents }: { contents: FileSystemItem[] | null }) {
         return (
           <li
             key={item.name}
-            className={`ml-4 flex gap-2 text-center items-center ${
+            className={`ml-4 flex gap-2 text-center items-center hover:text-ctp-base hover:bg-ctp-subtext0 transition-colors w-fit pr-5 pl-2 rounded-sm ${
               item.type === "directory" ? " font-bold" : "text-ctp-subtext0"
             } ${item.link ? "cursor-pointer underline" : ""}`}
             onClick={() => {
